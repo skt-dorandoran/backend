5f74b2c6ce521a859ace300dec418facf60f1192
unknown <richardyun01@gmail.com>
2026-02-05 15:14:34 +0900

    삭제) 디버그 출력문 주석 처리

--- routers/stt_router.py
+++ routers/stt_router.py
@@ -31,7 +31,7 @@ async def transcribe_ws(
     실시간 스트리밍: 클라이언트가 binary frame으로 오디오를 보내면
     transcript를 즉시 WS로 push
     """
-    print("WS route hit")
+    # print("WS route hit")
     await stream_transcribe_ws(
         websocket,
         encoding=encoding,
